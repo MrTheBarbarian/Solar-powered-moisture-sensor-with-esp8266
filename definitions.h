db4740d04487241be8832236efd778bc19b8ea94
MrTheBarbarian <miroslav.sotiroff@gmail.com>
2019-04-05 16:10:29 +0300

    Update definitions.h

--- definitions.h
+++ definitions.h
@@ -1,8 +1,8 @@
 #define SerialDebug false
 #define SleepTest false //  false/true
 int Out1 = 14; //output pin    
-const char* ssid     = "NASA";
-const char* password = "wG1@*_678940lNmsqo02761";
+const char* ssid     = "";
+const char* password = "";
 const char mqtt_clientid[] = "ESP_MoistureNode1"; // MQTT ClientID 
 const char mqtt_sub_topic[] = "Command_WaterSpliterContorller_Out1";
 int mqtt_host[4] = {192,168,100,5}; //Server IP
